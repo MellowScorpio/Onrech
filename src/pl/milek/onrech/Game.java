4db85e34677c67a86ca1df0b417dd217647febdc
Arkadiusz Milek <Arkadiusz.Milek@tomtom.com>
2017-03-06 16:33:39 +0100

    chapter 6 update

--- src/pl/milek/onrech/Game.java
+++ src/pl/milek/onrech/Game.java
@@ -1,27 +1,78 @@
 package pl.milek.onrech;
 
-public class Game implements Runnable {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
-    public static final int WIDTH = 300;
-    public static final int HEIGHT = WIDTH / 16 * 9;
-    public static final int SCALE = 3;
+public class Game extends Canvas implements Runnable {
+    private static final long serialVersionUID = 1L;
+
+    private static final String TITLE = "OnReach";
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = WIDTH / 16 * 9;
+    private static final int SCALE = 3;
 
     private Thread thread;
+    private JFrame frame;
+    private boolean running = false;
+
+    // executed once when we create object
+    public Game(){
+      Dimension size = new Dimension(WIDTH*SCALE, HEIGHT*SCALE);
+        setPreferredSize(size);
+
+        frame = new JFrame();
+    }
 
     public synchronized void start(){
+        running = true;
         thread = new Thread(this, "Display");
         thread.start();
     }
 
     public synchronized void stop(){
+        running = false;
         try {
             thread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    @Override
-    public void run() {
 
+    public void run() {
+        while(running){
+            update();
+            render();
+        }
+    }
+
+    private void update() {
+
+    }
+
+    private void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.dispose();
+        bs.show();
+    }
+
+    //this is the Genesis! runned when program starts
+    public static void main(String[] args){
+        Game game = new Game();
+        game.frame.setResizable(false);
+        game.frame.setTitle(TITLE);
+        game.frame.add(game);
+        game.frame.pack();
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.frame.setLocationRelativeTo(null);
+        game.frame.setVisible(true);
+
+        game.start();
     }
 }
